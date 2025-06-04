5c4945b3560ffa09c7ab8811fc5f90997aad37dd
javier.avrudsky@harness.io <javier.avrudsky@harness.io>
2025-06-04 18:08:42 -0300

    Adding guideline prompts to allow improving date parameter handling

---
+++ pkg/harness/prompts.go
@@ -0,0 +1,25 @@
+package harness
+
+import (
+	p "github.com/harness/harness-mcp/pkg/prompts"
+	"github.com/mark3labs/mcp-go/server"
+)
+
+
+// RegisterPrompts initializes and registers predefined prompts with the MCP server.
+func RegisterPrompts(mcpServer *server.MCPServer ) {
+	prompts := p.Prompts{} 
+
+	// This prompt is intended to make the LLM handle the date parameters in the correct format because fields descriptions where not enough.
+	prompts.Append(
+			p.NewPrompt().SetName("get_ccm_overview").
+			SetDescription("Ensure parameters are provided correctly and in the right format. ").
+			SetResultDescription("Input parameters validation").
+			SetText(`Before calling get_ccm_overview, ensure you have: accountIdentifier, groupBy, startDate, and endDate.
+					- If any are missing, ask the user for the specific value(s).
+					- Always send startDate and endDate as Unix‐epoch milliseconds.
+					- If no dates are supplied, default startDate to 60 days ago and endDate to now.`).
+			Build())
+
+	p.AddPrompts(prompts, mcpServer)
+}
