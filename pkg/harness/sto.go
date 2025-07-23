fd95b40f241dba961697366cb63d0f52fecb8567
Tarun Kumar Acharya <tarunkumar.acharya@harness.io>
2025-07-23 19:09:52 +0530

    feat: [SSCA-4077]: rebased to sampling changes

--- pkg/harness/sto.go
+++ pkg/harness/sto.go
@@ -9,7 +9,7 @@ import (
 	"github.com/harness/harness-mcp/client/sto/generated"
 	"github.com/harness/harness-mcp/cmd/harness-mcp-server/config"
 	"github.com/harness/harness-mcp/pkg/appseccommons"
-	builder "github.com/harness/harness-mcp/pkg/harness/event/scs"
+	builder "github.com/harness/harness-mcp/pkg/harness/event/common"
 	"github.com/mark3labs/mcp-go/mcp"
 	"github.com/mark3labs/mcp-go/server"
 )
