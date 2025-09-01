c936143dec2904cc60438404b038d499a59b16ed
harness-auto-fix <harness-auto-fix-ai@harness.io>
2025-09-01 13:01:09 +0000

    harness-auto-fix created this fix

--- pkg/types/enum/transport_type.go
+++ pkg/types/enum/transport_type.go
@@ -4,23 +4,23 @@ package enum
 type TransportType string
 
 const (
-    // TransportStdio represents standard input/output transport
-    TransportStdio TransportType = "stdio"
-    // TransportHTTP represents HTTP transport
-    TransportHTTP TransportType = "http"
+	// TransportStdio represents standard input/output transport
+	TransportStdio TransportType = "stdio"
+	// TransportHTTP represents HTTP transport
+	TransportHTTP TransportType = "http"
 )
 
 // String returns the string representation of the transport type
 func (t TransportType) String() string {
-    return string(t)
+	return string(t)
 }
 
 // ParseTransportType converts a string to a TransportType
 func ParseTransportType(s string) TransportType {
-    switch s {
-    case "http":
-        return TransportHTTP
-    default:
-        return TransportStdio
-    }
+	switch s {
+	case "http":
+		return TransportHTTP
+	default:
+		return TransportStdio
+	}
 }
