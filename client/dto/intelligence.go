cc71ed792f56e29023f13b22f6d2288f854669cb
Yogesh Chauhan <yogesh.chauhan@harness.io>
2025-10-09 14:17:19 +0530

    refactor: [ML-1508]: deprecate stage and step group action types in intelligence DTO

--- client/dto/intelligence.go
+++ client/dto/intelligence.go
@@ -73,22 +73,23 @@ const (
 func ActionTypeValues() []string {
 	return []string{
 		string(CreateStep),
-		string(CreateStage),
+		//ML-1508: Deprecating create/update stage
+		// string(UpdateStage),
+		// string(CreateStage),
+		// string(UpdateStepGroup),
+		// string(CreateStepGroup),
 		string(CreatePipeline),
 		string(CreateEnv),
 		string(CreateSecret),
 		string(CreateService),
 		string(CreateConnector),
-		string(CreateStepGroup),
 		string(CreateProcess),
 		string(UpdateStep),
-		string(UpdateStage),
 		string(UpdatePipeline),
 		string(UpdateEnv),
 		string(UpdateSecret),
 		string(UpdateService),
 		string(UpdateConnector),
-		string(UpdateStepGroup),
 	}
 }
 
